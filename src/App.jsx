1dad52fb96013abfe057dc2a1ddcff4faaf723cb
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 01:22:25 +0700

    Integrate Core Concepts section with dynamic content
    - Import CORE_CONCEPTS data from ./data.js for dynamic rendering
    - Add CoreConcept component for individual concept representation
    - Utilize CoreConcept component within App to render CORE_CONCEPTS data
    - Implement dynamic rendering of core concepts in the App component
    - Ensure modular and reusable component structure for core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -18,12 +19,34 @@ function Header() {
   );
 }
 
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
