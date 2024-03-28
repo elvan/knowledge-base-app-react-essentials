00171e03ccef21c0e9cd5efe27cd9e55086a7ce3
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 23:21:59 +0700

    Dynamically render examples based on tab selection
    - Update initial selectedTopic state to 'components' for default display
    - Import EXAMPLES data from ./data.js for dynamic content rendering
    - Render selected topic's title, description, and code in Examples section
    - Utilize EXAMPLES object to fetch content based on selectedTopic state
    - Enhance interactivity and educational value of Examples section

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
 import TabButton from './components/TabButton.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -38,7 +38,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
