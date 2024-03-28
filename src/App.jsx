cff5c310169d0053c765300d9327d3638ae8568a
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 23:30:44 +0700

    Refine dynamic content rendering logic in App
    - Remove default selectedTopic state to prompt initial topic selection
    - Implement conditional rendering for tabContent based on selectedTopic
    - Display a prompt message when no topic is selected by the user
    - Dynamically render selected topic's content only upon selection
    - Improve user experience by guiding initial interaction with examples

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,27 @@ import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
 
   console.log('APP COMPONENT EXECUTING');
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -38,13 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
