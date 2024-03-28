2ba655f137753a88f06de53c61675f45e4e601c3
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 22:45:53 +0700

    Enhance TabButton with click event logging
    - Add handleClick function to TabButton component
    - Log 'Hello World!' to console on button click
    - Ensure interactive feedback for TabButton clicks

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log('Hello World!');
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
