3363cdefc41c2fd74262b3f3d33894c0143b96b1
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 22:38:36 +0700

    Add TabButton component and integrate in App
    - Introduce TabButton component for menu in Examples section
    - Use TabButton component in App to create menu buttons
    - Render Examples section with menu buttons for interactive tabs
    - Organize Examples section to demonstrate React concepts dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS } from './data.js';
 
 function App() {
@@ -19,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
