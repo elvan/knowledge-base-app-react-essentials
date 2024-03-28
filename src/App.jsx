a973946c4922b9fe1b9972784e3443868582b5f0
Elvan Hidayat <elvanhd@gmail.com>
2024-03-28 22:29:26 +0700

    Organize Header styles and update component path
    - Move Header component to a dedicated folder with its CSS
    - Introduce Header.css for styling the Header component
    - Adjust import path in App.jsx to reflect new Header location
    - Remove Header specific styles from src/index.css
    - Enhance modularity and separation of concerns in project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import CoreConcept from './components/CoreConcept.jsx';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data.js';
 
 function App() {
